cf3bd82d5d146f67347571fe1efd5ee361fcbfb6
AjayThombare <thombareajay2901@gmail.com>
2024-10-01 00:57:27 +0530

    Single dimensional array

---
+++ 23 Array 1D/pr3.java
@@ -0,0 +1,17 @@
+
+class P3{
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+
+		for (int i=1; i<=5; i++){
+			System.out.println("Enter jersey no for player : ");
+			int jerNo = sc.nextInt();
+		}
+		/* 
+		 System.out.println("JerNo  : "+jerNo1);
+		 System.out.println("JerNo  : "+jerNo2);
+		 System.out.println("JerNo  : "+jerNo3);
+		 System.out.println("JerNo  : "+jerNo4);
+		 System.out.println("JerNo  : "+jerNo5);*/
+	}
+}
---
+++ 23 Array 1D/pr4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class P4{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+
+		int jreNo = 0;
+		for (int i=1; i<=5; i++){
+			System.out.println(" Enter Jersey no for player ");
+			int jerNo = sc.nextInt();
+			System.out.println("JerNo : " + jerNo);
+		}
+		/*
+		System.out.println("jerNo : " + jerNo1);
+		System.out.println("jerNo : " + jerNo2);
+		System.out.println("jerNo : " + jerNo3);
+		System.out.println("jerNo : " + jerNo4);
+		System.out.println("jerNo : " + jerNo5);*/
+	}
+}
+
